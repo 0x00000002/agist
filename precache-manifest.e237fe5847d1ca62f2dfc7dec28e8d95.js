d2a4b0f23ace22565fb892302781f6180c1f88a9
Alex Tikonoff <tikonoff@gmail.com>
2018-11-16 14:55:37 +1300

    Updates

---
+++ precache-manifest.e237fe5847d1ca62f2dfc7dec28e8d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05b892c7c1ee638e70a",
+    "url": "/agist/static/js/runtime~main.e05b892c.js"
+  },
+  {
+    "revision": "43457a6d3925600e1913",
+    "url": "/agist/static/js/main.43457a6d.chunk.js"
+  },
+  {
+    "revision": "0e9f99f8da69bac79574",
+    "url": "/agist/static/js/1.0e9f99f8.chunk.js"
+  },
+  {
+    "revision": "43457a6d3925600e1913",
+    "url": "/agist/static/css/main.038fc69d.chunk.css"
+  },
+  {
+    "revision": "0e9f99f8da69bac79574",
+    "url": "/agist/static/css/1.19470d9f.chunk.css"
+  },
+  {
+    "revision": "a2b2c2fbeb590cc94efd09694cdb7bbb",
+    "url": "/agist/index.html"
+  }
+];
